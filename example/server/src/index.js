4c8133b357ed96634bb915d1ce2ba1a435f9de03
Chris Oloff <christian.oloff@gmail.com>
2016-07-20 10:02:00 +0200

    added server example, untested

---
+++ example/server/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { Server as HttpServer } from 'http'
+import SocketIO from 'socket.io'
+
+const app = express()
+const server = HttpServer(app)
+
+const io = SocketIO(server)
+
+const things = io.of('/things')
+things.on('connection', socket => {
+  console.log('connected to things')
+})
+
+setInterval(() => {
+  things.emit('now', { now: new Date() })
+}, 1000)
+
+const port = process.env.PORT || 3000
+server.listen(port, () => console.log(`listening on port ${ port }`))
